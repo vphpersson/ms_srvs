ca05a671c1b822a6a32b48ecffee1cef3f156fb5
Viktor Persson <12910623+vphpersson@users.noreply.github.com>
2019-12-23 15:52:43 +0100

    Added pipe name constant.

--- ms_srvs/__init__.py
+++ ms_srvs/__init__.py
@@ -7,3 +7,5 @@ MS_SRVS_ABSTRACT_SYNTAX: Final[PresentationSyntax] = PresentationSyntax(
     if_uuid=UUID('4b324fc8-1670-01d3-1278-5a47bf6ee188'),
     if_version=3
 )
+
+MS_SRVS_PIPE_NAME: Final[str] = 'srvsvc'
