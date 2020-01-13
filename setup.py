98faf1e74a94db2aa2632eb7e177c25d583a9ba1
Viktor Persson <vph.persson@gmail.com>
2020-01-13 22:45:31 +0100

    Made compatible with `Win32Error`.

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
+
 setup(
     name='ms_srvs',
     version='0.1',
     packages=find_packages(),
     install_requires=[
-        'rpc @ http://github.com/vphpersson/rpc/tarball/master'
+        'rpc @ http://github.com/vphpersson/rpc/tarball/master',
+        'msdsalgs @ https://github.com/vphpersson/msdsalgs/tarball/master'
     ]
 )
